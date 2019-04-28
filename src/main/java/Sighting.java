85b430b59438fcdd7cd4513590a3cabf88a8ddb2
pwachira900@gmail.com <pwachira900@gmail.com>
2019-04-28 16:32:04 +0300

    add Sighting class constructor

--- src/main/java/Sighting.java
+++ src/main/java/Sighting.java
@@ -1,2 +1,76 @@
-public class Sighting {
+import java.sql.Timestamp;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import org.sql2o.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Sighting implements DatabaseManagement {
+
+    private int id;
+    private int animal_id;
+    private String location;
+    private String ranger_name;
+    private Timestamp timestamp;
+
+    // constructor for sighting which implements abstract method save in Database management class
+
+    public Sighting(int animal_id, String location, String ranger_name) {
+        if (ranger_name.equals("")) {
+            throw new IllegalArgumentException("Please enter Ranger name.");
+        }
+        this.animal_id = animal_id;
+        this.location = location;
+        this.ranger_name = ranger_name;
+        this.save();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
