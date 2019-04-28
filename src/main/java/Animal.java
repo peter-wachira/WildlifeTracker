48221faf5ec6be6cbef3e5d89d5c82c603f36bd7
pwachira900@gmail.com <pwachira900@gmail.com>
2019-04-28 15:35:28 +0300

    add animal constructor

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -4,4 +4,57 @@ import java.util.ArrayList;
 import java.sql.Timestamp;
 
 public class Animal {
+    public int id;
+    public String name;
+    public String type;
+    //creating animal with constant non endangered
+    public static final String ANIMAL_TYPE = "Non-endangered";
+
+    public Animal(String name){
+        if (name.equals("")){
+            //throw exception if no name is entered
+            throw new IllegalArgumentException("Please enter an animal name.");
+        }
+        this.name = name;
+        type = ANIMAL_TYPE;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
