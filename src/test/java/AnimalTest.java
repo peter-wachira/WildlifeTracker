1f007cd32a87d992562cad11867171b4c0022bac
pwachira900@gmail.com <pwachira900@gmail.com>
2019-04-28 15:46:36 +0300

    add database rule

--- src/test/java/AnimalTest.java
+++ src/test/java/AnimalTest.java
@@ -1,2 +1,65 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.sql2o.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class AnimalTest {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
