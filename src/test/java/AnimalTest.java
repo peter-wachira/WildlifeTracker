8ab7969c7ebe87ea69d15a966dba59cc27db1195
pwachira900@gmail.com <pwachira900@gmail.com>
2019-04-28 16:12:39 +0300

    add get methods for endangered animals

--- src/test/java/AnimalTest.java
+++ src/test/java/AnimalTest.java
@@ -1,32 +1,3 @@
-import org.junit.*;
-import static org.junit.Assert.*;
-import org.sql2o.*;
-import java.util.ArrayList;
-import java.util.List;
-
-public class AnimalTest {
-
-
-    @Rule
-    public DatabaseRule database = new DatabaseRule();
-
-    private Animal testAnimal;
-
-
-    @Before
-    public void setup(){
-        testAnimal = new Animal("Orangutan","");
-    }
-
-    @Test
-    public void animal_instantiatesCorrectly_true() {
-        assertEquals(true, testAnimal instanceof Animal);
-    }
-
-
-
-
-
 
 
 
