e73eb42a4a3af000c9b7f9f371d8fedb2d8ab77c
pwachira900@gmail.com <pwachira900@gmail.com>
2019-04-28 22:15:44 +0300

    added animal edit form

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,45 @@
+import java.util.HashMap;
+import java.util.Map;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+
+
 public class App {
+
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+        String layout = "templates/layout.vtl";
+
+
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
+
+        port(port);
+        // retrieving index page
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("template", "templates/index.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+        //retrieving animals
+        get("/animals", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("animals", Animal.all());
+            model.put("template", "templates/animals.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+
+
+
+
+    }
+
 }
---
+++ src/main/resources/public/app.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+// Animations init
+new WOW().init();
+
+var typed=new Typed(`#type`,{
+     strings:[" Wildlife Tracker "," An application that allows Rangers to track wildlife sightings in the area. "],
+     backSpeed:70,
+     typeSpeed:80,
+     smartBackspace:true,
+     loop: true,
+   })
+   var typed =new Typed(`#msg`,{
+        strings:[" Your Animal has been saved ! "],
+        backSpeed:70,
+        typeSpeed:80,
+        smartBackspace:true,
+      })
+})
+
+
+function secondClick(){
+timer:4000
+Swal.fire(
+  'View Animals?',
+)
+}
+
+function firstClick(){
+timer:4000
+Swal.fire(
+  'View Sightings?',
+
+)
+}
+
+
+
+
+
+
+
+
+// Animations init
+new WOW().init();
